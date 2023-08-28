590a8730506f29a682f1163c93575fee93b8fac3
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-28 09:50:42 +0530

    Create Department.java
    This is a class Department which has 2 variables - name and number   and 2 functions .

---
+++ Department.java
@@ -0,0 +1,22 @@
+public class Department{
+   private String name;
+   private int num;
+   public void set(String name, int num){
+       this.name=name;
+       this.num=num;
+   }
+   public void get(){
+      System.out.println(name);
+      System.out.println(num);
+   }
+ public static void main(String [] args){
+    Department d1= new Department();
+    Department d2= new Department();
+    d1.set("CSIT",240);
+    System.out.println("The department and strength respectively is ");
+    d1.get();
+    d1.set("AIML",120);
+    System.out.println("The department and strength respectively is ");
+    d1.get();
+ }
+}
