859326d6e7d1855411572dcc2d0389954b54cb30
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 19:05:28 +0530

    Question10.java

---
+++ Question10.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Question10 {
+    public static void main(String[] args) {
+        System.out.println("The alphabets from A to Z is ");
+       for(char ch='A';ch<='Z';ch++){
+            System.out.println(ch);
+       }
+    }
+}
