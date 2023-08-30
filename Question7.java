b83da5e34e8c13e032d0847d7416561ea8de14fe
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 18:49:59 +0530

    Question7.java

---
+++ Question7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Question7 {
+    public static void main(String[] args) {
+        int n;
+        int sum=0;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter value of n` ");
+        n = s.nextInt();
+       for(int i=1;i<=n;i++){
+           sum=sum+i;
+       }
+       System.out.println("The sum of natural numbers upto n is "+sum);
+    }
+}
