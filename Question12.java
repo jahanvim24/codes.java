9f9814e345c4a99617f7406facb8f1acf91879a8
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 19:06:40 +0530

    Question12.java

---
+++ Question12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Question12 {
+    public static void main(String[] args) {
+        System.out.println("Enter any number ");
+        Scanner s=new Scanner(System.in);
+        int n;
+        int rev=0;
+        n=s.nextInt();
+        while(n>0){
+            int a=n%10;
+            rev=(rev*10)+a;
+            n=n/10;
+        }
+        System.out.println("the number after reversing is "+rev);
+    }
+}
