de98c91e7fac7a228b34820b538b1761d2f52c2d
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 19:50:09 +0530

    Question11.java

---
+++ Question11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  
+public class Question11{  
+    public static void main(String[] args)    
+    {   
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter the first number: ");  
+        int n1 = sc.nextInt();  
+        System.out.println("Enter the second number: ");  
+        int n2 = sc.nextInt();  
+        int gcd=0;
+        for(int i = 1; i <= n1 || i <= n2; i++) 
+        {
+         if( n1%i == 0 && n2%i == 0 )
+            gcd = i;
+      }
+      System.out.println("Gcd of numbers is "+gcd);
+   }  
+}  
