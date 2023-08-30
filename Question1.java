5c60aaaf2193a729ba41f9085e1c4ce52f73c593
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 18:38:20 +0530

    Question1.java

---
+++ Question1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Question1{
+public static void main(String []args){
+int a;
+Scanner s= new Scanner(System.in);
+System.out.println("Enter a number ");
+a=s.nextInt();
+System.out.println("The entered number is "+a);
+}
+}
