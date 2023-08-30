5ff3fcd63af1112f0c128731ac61000b8a34c39a
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 18:45:10 +0530

    Question5.java

---
+++ Question5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Question5 {
+    public static void main(String[] args) {
+        char ch;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter a alphabet ");
+        ch = s.next().charAt(0);
+        if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+            System.out.println("The alphabet is vowel ");
+        }
+        else{
+             System.out.println("The alphabet is consonant  ");
+        }
+    }
+}
