b30f0a6171bc47f3bcbfc48631d59a54f06ffbaa
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 19:04:22 +0530

    Question9.java

---
+++ Question9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Question9 {
+    public static void main(String[] args) {
+        int n;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter a number` ");
+        n = s.nextInt();
+        System.out.println("The table of number is ");
+       for(int i=1;i<=10;i++){
+            System.out.println(+n+" * "+i+" = "+(n*i));
+       }
+    }
+}
