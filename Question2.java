6864657cb6083dcb05a2e2172d0fe068e31cf8af
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 19:43:31 +0530

    Question2.java

---
+++ Question2.java
@@ -0,0 +1,21 @@
+class Question2 {
+    public static void main(String[] args) {
+        boolean a=true;
+        char b='a';
+        byte c=9;
+        short d=2;
+        int e=54444;
+        long f=432323423;
+        float g = 32;
+        double h =83213.222;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
