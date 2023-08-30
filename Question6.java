c3996162d90b4b8fa0ab62f3c252d6fce9cef4d5
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-30 18:48:37 +0530

    Question6.java

---
+++ Question6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Question6 {
+    public static void main(String[] args) {
+        int a;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter a number ");
+        a = s.nextInt();
+        if(a>0){
+            System.out.println("The number is positive ");
+        }
+        else{
+             System.out.println("The number is negative ");
+        }
+    }
+}
