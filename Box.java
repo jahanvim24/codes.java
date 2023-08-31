7a35e263f68e860fd439edce315887376abf0e42
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-31 19:38:49 +0530

    Box.java
    This class Box contains a set and get method and a volume method which calculates the volume.

--- Box.java
+++ Box.java
@@ -2,24 +2,28 @@ public class Box{
    private int height;
    private int length;
    private int breadth;
-  Box(){
-      height=0;
-      length=0;
-      breadth=0;
-  }
-  Box(int height,int length,int breadth){
+  
+   public void set(int height,int length,int breadth){
       this.height=height;
       this.length=length;
       this.breadth=breadth;
+  }
+  public void get(){
+     System.out.println(height);
+     System.out.println(length);
+     System.out.println(breadth);
+     
   }
   public int volume(){
       return(length*breadth*height);
   }
 public static void main(String [] args){
     Box cuboid1= new Box();
+    cuboid1.set(10,20,30);
+    System.out.println("The dimensions are  : ");
+    cuboid1.get();
     System.out.println("The area of cuboid 1 is "+cuboid1.volume());
-    Box cuboid2 = new Box(10,15,20);
-    System.out.println("The area of cuboid 2 is "+cuboid2.volume());
+    
 }
    
 }
