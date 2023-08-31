d3ebfc66a52d982465f2dc7ba543f1ce49664b60
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-08-31 19:00:53 +0530

    UppAlphabet.java
    This class prints all alphabets from A to Z in uppercase.

--- UppAlphabet.java
+++ UppAlphabet.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Question10 {
+class UppAlphabet {
     public static void main(String[] args) {
         System.out.println("The alphabets from A to Z is ");
        for(char ch='A';ch<='Z';ch++){
