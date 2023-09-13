977082b536206839154323bf51730cce767f3c92
jahanvim24 <143196227+jahanvim24@users.noreply.github.com>
2023-09-13 11:39:48 +0530

    Create 32).String basic.java

---
+++ 32).String basic.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+       String first="Java";
+       String second="Python";
+        String third="C++";
+        String s1 = new String("JAVA");
+        String s2 = new String("pYTHON");
+        String s3 = new String("C++");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        
+    }
+}
